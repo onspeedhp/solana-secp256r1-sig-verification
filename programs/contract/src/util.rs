24f7de2fb634e86c860e62cf3d3f7817f4177539
chaucaliber <chau@caliber.build>
2025-02-04 03:06:44 +0700

    first commit

---
+++ programs/contract/src/util.rs
@@ -0,0 +1,64 @@
+use crate::error::ErrorCode;
+use anchor_lang::{prelude::*, solana_program::instruction::Instruction};
+
+const SECP256R1_ID: Pubkey = pubkey!("Secp256r1SigVerify1111111111111111111111111");
+
+pub fn verify_secp256r1_ix(ix: &Instruction, pubkey: &[u8], msg: &[u8], sig: &[u8]) -> Result<()> {
+    if ix.program_id       != SECP256R1_ID                 ||  // The program id we expect
+        ix.accounts.len()   != 0                            ||  // With no context accounts
+        ix.data.len()       != (2 + 14 + 33 + 64 + msg.len())
+    // And data of this size
+    {
+        return Err(ErrorCode::SigVerificationFailed.into()); // Otherwise, we can already throw err
+    }
+
+    check_secp256r1_data(&ix.data, pubkey, msg, sig)?; // If that's not the case, check data
+
+    Ok(())
+}
+
+pub fn check_secp256r1_data(data: &[u8], pubkey: &[u8], msg: &[u8], sig: &[u8]) -> Result<()> {
+    // Parse header components
+    let num_signatures = &[data[0]]; // Byte 0
+    let signature_offset = &data[2..=3]; // Bytes 2-3
+    let signature_instruction_index = &data[4..=5]; // Bytes 4-5
+    let public_key_offset = &data[6..=7]; // Bytes 6-7
+    let public_key_instruction_index = &data[8..=9]; // Bytes 8-9
+    let message_data_offset = &data[10..=11]; // Bytes 10-11
+    let message_data_size = &data[12..=13]; // Bytes 12-13
+    let message_instruction_index = &data[14..=15]; // Bytes 14-15
+
+    // Get actual data
+    let data_pubkey = &data[16..16 + 33]; // 33 bytes public key
+    let data_sig = &data[49..49 + 64]; // 64 bytes signature
+    let data_msg = &data[113..]; // Variable length message
+
+    // Calculate expected values
+    const SIGNATURE_OFFSETS_SERIALIZED_SIZE: u16 = 14;
+    const DATA_START: u16 = 2 + SIGNATURE_OFFSETS_SERIALIZED_SIZE;
+    let msg_len: u16 = msg.len() as u16;
+    let pubkey_len: u16 = pubkey.len() as u16;
+    let sig_len: u16 = sig.len() as u16;
+
+    let exp_pubkey_offset: u16 = DATA_START;
+    let exp_signature_offset: u16 = DATA_START + pubkey_len;
+    let exp_message_data_offset: u16 = exp_signature_offset + sig_len;
+
+    // Verify header
+    if num_signatures != &[1]
+        || signature_offset != &exp_signature_offset.to_le_bytes()
+        || signature_instruction_index != &0xFFFFu16.to_le_bytes()
+        || public_key_offset != &exp_pubkey_offset.to_le_bytes()
+        || public_key_instruction_index != &0xFFFFu16.to_le_bytes()
+        || message_data_offset != &exp_message_data_offset.to_le_bytes()
+        || message_data_size != &msg_len.to_le_bytes()
+        || message_instruction_index != &0xFFFFu16.to_le_bytes()
+    {
+        return Err(ErrorCode::SigVerificationFailed.into());
+    }
+
+    if &data_pubkey[..] != &pubkey[..] || &data_sig[..] != &sig[..] || &data_msg[..] != &msg[..] {
+        return Err(ErrorCode::SigVerificationFailed.into());
+    }
+    Ok(())
+}
---
+++ tests/contract.ts
@@ -0,0 +1,86 @@
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Contract } from '../target/types/contract';
+import ECDSA from 'ecdsa-secp256r1';
+import { createSecp256r1Instruction } from './util';
+import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
+import { publicKey } from '@coral-xyz/anchor/dist/cjs/utils';
+import dotenv from 'dotenv';
+import bs58 from 'bs58';
+dotenv.config();
+
+describe('contract', () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Contract as Program<Contract>;
+
+  const anchorProvider = anchor.getProvider() as anchor.AnchorProvider;
+
+  const wallet = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY!));
+
+  it('Is initialized!', async () => {
+    const privateKey = ECDSA.generateKey();
+
+    const message = 'Hello';
+
+    const messageBytes = Buffer.from(message);
+
+    const publicKeyBase64 = privateKey.toCompressedPublicKey();
+    const pubkey = Buffer.from(publicKeyBase64, 'base64');
+
+    const signatureBase64 = privateKey.sign(Buffer.from(message).toString());
+    let signature = Buffer.from(signatureBase64, 'base64');
+
+    const instructionData = createSecp256r1Instruction(
+      privateKey,
+      messageBytes,
+      pubkey,
+      signature
+    );
+
+    const transferSOLInstruction = SystemProgram.transfer({
+      fromPubkey: wallet.publicKey,
+      toPubkey: Keypair.generate().publicKey, 
+      lamports: 1000000,
+    });
+
+    const remainingAccounts = transferSOLInstruction.keys;
+
+    remainingAccounts.push({
+      pubkey: transferSOLInstruction.programId,
+      isSigner: false,
+      isWritable: false,
+    });
+
+    const tx = new Transaction().add(instructionData).add(
+      await program.methods
+        .verifySecpr1(
+          Array.from(pubkey),
+          messageBytes,
+          Array.from(signature),
+          transferSOLInstruction.programId,
+          transferSOLInstruction.data
+        )
+        .accounts({
+          sender: wallet.publicKey,
+        })
+        .remainingAccounts(remainingAccounts)
+        .instruction()
+    );
+
+    tx.recentBlockhash = (
+      await anchorProvider.connection.getLatestBlockhash()
+    ).blockhash;
+
+    tx.feePayer = wallet.publicKey;
+
+    tx.partialSign(wallet);
+
+    const sig = await anchorProvider.connection.sendTransaction(tx, [wallet], {
+      skipPreflight: true,
+    });
+
+    console.log(sig);
+  });
+});
